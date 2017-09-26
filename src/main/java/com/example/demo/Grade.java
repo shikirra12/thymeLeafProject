c91686f7c964700f6887b0592bbe8f07a7f57324
Shikirra Williams <shikirra.williams@gmail.com>
2017-09-26 15:37:28 -0400

    finished project

---
+++ src/main/java/com/example/demo/Grade.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public enum Grade {
+    First,
+    Second,
+    Third,
+    Fourth,
+    Fifth,
+    Sixth,
+    Seventh,
+    Eighth,
+    Ninth,
+    Tenth,
+    Eleventh,
+    Twelfth;
+}
